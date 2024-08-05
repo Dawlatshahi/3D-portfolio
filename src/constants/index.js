9eae0499dfbc6e2461dfda717c9d9cf3dec49c95
Farzana Asadi <121952162+Dawlatshahi@users.noreply.github.com>
2024-08-05 19:04:17 +0300

    mini updates

--- src/constants/index.js
+++ src/constants/index.js
@@ -216,7 +216,7 @@ const projects = [
 			},
 		],
 		image: carrent,
-		source_code_link: 'https://github.com/',
+		source_code_link: 'https://github.com/Dawlatshahi/ourChat',
 	},
 	{
 		name: 'Job IT',
