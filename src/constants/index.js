b8a4832141d0ba9ed91a3ad70d630e7b1bb5a1b3
Farzana Asadi <121952162+Dawlatshahi@users.noreply.github.com>
2024-08-05 18:14:43 +0300

    Feedback and works projects done.

--- src/constants/index.js
+++ src/constants/index.js
@@ -198,16 +198,16 @@ const testimonials = [
 
 const projects = [
 	{
-		name: 'Car Rent',
+		name: 'Chat App',
 		description:
-			'Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.',
+			'Web-based platform that allows users to chat seamlessly, schedule, and manage appointments with service providers, offering a convenient and efficient solution for communication and scheduling needs.',
 		tags: [
 			{
-				name: 'react',
+				name: 'Next.js',
 				color: 'blue-text-gradient',
 			},
 			{
-				name: 'mongodb',
+				name: 'prisma',
 				color: 'green-text-gradient',
 			},
 			{
